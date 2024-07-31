924852c1ca18ad1518decd7df363af367dc30fdc
Devesh Shrestha <deveshshrestha20@gmail.com>
2024-07-31 20:42:32 +0545

    vite last change

--- MemoryGame/vite.config.js
+++ MemoryGame/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Memory-Card-Game/", // Ensure this matches your repository name
+  base: "/Memory-Card-Game/",
   server: {
     port: 2500,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash].[ext]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
+  transformIndexHtml: {
+    transform: (html) =>
+      html.replace(/<head>/, `<head><base href="/Memory-Card-Game/">`),
   },
 });
